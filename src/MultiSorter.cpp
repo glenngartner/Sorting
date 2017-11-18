19aaacfd41a1c90ddebad6c974a1f20b2ba926c6
Glenn Gartner <glenngartner@gmail.com>
2017-11-17 23:25:10 -0500

    begin insertion method

--- src/MultiSorter.cpp
+++ src/MultiSorter.cpp
@@ -6,4 +6,26 @@
 
 MultiSorter::MultiSorter(std::vector<int> *sourceList) {
     this->sourceList = sourceList;
+    this->sortedList = {};
+}
+
+void MultiSorter::insertion() {
+    // clear the sorted list
+    this->sortedList = {};
+    // add the first element of the source list to the sorted list
+    this->sortedList.push_back((*this->sourceList)[0]);
+
+    // for every item in the sorted list collection (skip the first one)
+    for (int i = 1; i < this->sourceList->size(); i++) {
+        // initialize the sorted list index to 0 (the beginning)
+        int sortedIndex = 0;
+
+        while (sortedIndex < this->sortedList.size()) {
+            // if the current item in the source list is less than the current item in the sorted list
+            if ((*this->sourceList)[i] <= this->sortedList[sortedIndex]){
+                // insert the source list item before the sorted list item
+//                this->sortedList.insert()
+            }
+        };
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,7 @@ int main() {
     std::unique_ptr<RandomListGenerator> newList = std::make_unique<RandomListGenerator>(10, 0, 200, false);
     std::unique_ptr<MultiSorter> newSorter = std::make_unique<MultiSorter>(&newList->list);
 
+    newSorter->insertion();
+
     return 0;
 }
